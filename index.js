48bca87032ae9f3de6278b29e45f279d8685f9fb
Rakib <rh.rakibhasan365@gmail.com>
2024-10-11 02:01:37 +0600

    doctor add, update, delete success

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ async function run() {
         }
 
         const verifyToken = (req, res, next) => {
-            console.log('indside the verify token', req.headers.authorization);
 
             if (!req.headers.authorization) {
                 return res.status(401).send({ message: 'unauthorized access' })
@@ -68,7 +67,7 @@ async function run() {
         }
 
         // users related api
-        app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
@@ -86,7 +85,7 @@ async function run() {
                 admin = user?.role === 'admin'
             }
             res.send({ admin });
-        })
+        });
 
         // users post to database
         app.post("/users", async (req, res) => {
@@ -101,7 +100,7 @@ async function run() {
         });
 
         // check admin role and create admin
-        app.patch("/users/admin/:id",verifyToken, verifyAdmin, async (req, res) => {
+        app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -111,33 +110,72 @@ async function run() {
             }
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
-        })
+        });
 
         // user deleted
-        app.delete("/users/:id",verifyToken, verifyAdmin, async (req, res) => {
+        app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
         // doctors related apis
         app.get("/doctors", async (req, res) => {
             const result = await doctorsCollection.find().toArray();
             res.send(result);
-        })
+        });
 
         app.get("/doctors/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await doctorsCollection.findOne(query);
             res.send(result);
+        });
+
+        // add a doctors from admin 
+        app.post("/doctors", verifyToken, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        });
+
+        app.patch("/doctors/:id", verifyToken, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    name: doctor.name,
+                    category: doctor.category,
+                    specialist: doctor.specialist,
+                    experience: doctor.experience,
+                    chamberLocation: doctor.chamberLocation,
+                    rating: parseFloat(doctor.rating),
+                    about: doctor.about,
+                    education: doctor.education,
+                    overview: doctor.overview,
+                    phone: doctor.phone,
+                    email: doctor.email,
+                    image: doctor.image,
+                    visitFee: doctor.visitFee
+                }
+            }
+            const result = await doctorsCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        });
+
+        app.delete("/doctors/:id", verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await doctorsCollection.deleteOne(query);
+            res.send(result);
         })
 
         app.get("/reviews", async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
-        })
+        });
 
         // Appointment collection
         app.get("/appointment", async (req, res) => {
@@ -151,14 +189,14 @@ async function run() {
             const appointmentDoctor = req.body;
             const result = await appointmentCollection.insertOne(appointmentDoctor);
             res.send(result);
-        })
+        });
 
         app.delete("/appointment/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await appointmentCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
